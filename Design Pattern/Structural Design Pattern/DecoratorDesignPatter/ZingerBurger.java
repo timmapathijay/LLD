115b4876a6c5f974efadbd9ef3e4562205309937
T Jay Lakshmi <timmapathi.laxmi1609@gmail.com>
2025-04-30 21:52:48 +0530

    Create ZingerBurger.java

---
+++ Design Pattern/Structural Design Pattern/DecoratorDesignPatter/ZingerBurger.java
@@ -0,0 +1,12 @@
+public class ZingerBurger extends  Burger{
+
+    @Override
+    public String getBurgerDescription() {
+        return "Zinger Burger";
+    }
+
+    @Override
+    public double getBurgerCost() {
+        return 180.0;
+    }
+}
