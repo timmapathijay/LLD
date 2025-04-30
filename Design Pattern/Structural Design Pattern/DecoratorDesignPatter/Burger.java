e6ff53ff31a5caf998d5ef4a676c311b298ac1b4
T Jay Lakshmi <timmapathi.laxmi1609@gmail.com>
2025-04-30 21:49:59 +0530

    Create Burger.java

---
+++ Design Pattern/Structural Design Pattern/DecoratorDesignPatter/Burger.java
@@ -0,0 +1,6 @@
+public abstract class Burger {
+    private String burgerDescription;
+    private double burgerCost;
+    public abstract String getBurgerDescription();
+    public abstract  double getBurgerCost();
+}
